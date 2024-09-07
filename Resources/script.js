e46603d26d2854ffef4472e5eac99c36cc081267
Andrea Zummerova <adkabodiova@gmail.com>
2024-09-07 15:18:35 +0200

    "all effects"

--- Resources/script.js
+++ Resources/script.js
@@ -4,5 +4,13 @@ let everythingForEffects = document.getElementsByClassName('effect');
 for (let text of everythingForEffects) {
 
     text.innerHTML = text.innerText.split('').map((letters, i) => `<span style="
-    transition-delay:${i*20}ms">${letters}</span>`).join('');}
+    transition-delay:${i*20}ms; filter:hue-rotate(${i*8}deg">${letters}</span>`).join('');}
+
+let footerText = document.getElementById('copy');
+
+footerText.innerHTML = footerText.innerText.split('').map((letters, i) => `<span style="filter:hue-rotate(${i*5}deg">${letters}</span>`).join('');
+
+
+
+
 
